1b3014642d636c7f14adea8f46a0303b44d87342
Diwash Karki <diwaskarki96@gmail.com>
2023-12-21 22:13:09 +0545

    todo.api

--- index.js
+++ index.js
@@ -3,11 +3,12 @@ dotenv.config();
 const express = require("express");
 const mongoose = require("mongoose");
 const indexRouter = require("./routes/index");
+const bodyParser = require("body-parser");
 const app = express();
 
 const PORT = process.env.PORT;
 const DB_URL = process.env.DB_URL;
-
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.json());
 mongoose.connect(DB_URL).then(() => {
   console.log("Database is connected..");
